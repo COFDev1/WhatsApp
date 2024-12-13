fdf511c31a21c5819beaf3c431016f57f1808c7c
jessealvessilva <67879529+jessealvessilva@users.noreply.github.com>
2024-12-13 16:00:20 -0300

    New App: Versão teste

--- lib/view/logins_screen.dart
+++ lib/view/logins_screen.dart
@@ -5,29 +5,47 @@ import 'package:http/http.dart' as http;
 import 'list_customers.dart';
 import 'dart:convert';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<Login> createState() => _LoginState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginState extends State<Login> {
-  final List<ListCustomers> _listCustomers = [];
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final user = TextEditingController();
+  final senha = TextEditingController();
 
-  late final login = TextEditingController();
-  late final password = TextEditingController();
+  bool isLogin = true;
+  late String titulo;
+  late String actionButton;
+  late String toggleButton;
+  bool loading = false;
 
-  void geraTokenProtheus() async {
-    const url = 'http://192.168.2.12:8083/rest/app/customers/123';
+  @override
+  void initState() {
+    super.initState();
+    setFormAction(true);
+  }
+
+  setFormAction(bool acao) {
+    setState(() {
+      isLogin = acao;
+      if (isLogin) {
+        titulo = 'Bem vindo';
+        actionButton = 'Login';
+        toggleButton = 'Ainda não tem conta? Cadastre-se agora.';
+      } else {
+        titulo = 'Crie sua conta';
+        actionButton = 'Cadastrar';
+        toggleButton = 'Voltar ao Login.';
+      }
+    });
+  }
+
+  void login() async {
     String token = '';
-
-    // Navigator.pushReplacement(
-    //     context,
-    //     MaterialPageRoute(
-    //         builder: (BuildContext context) => const ListCustomers()));
-
-    // // Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const ListaClientesScreen()));
 
     ControlaUsuario conexao = ControlaUsuario();
 
@@ -44,7 +62,7 @@ class _LoginState extends State<Login> {
         context: context,
         builder: (ctx) => AlertDialog(
           title: const Text("Ocorreu um erro!"),
-          content: const Text("Falha ao autenticar"),
+          content: const Text("Falha ao conectar/autenticar"),
           actions: [
             TextButton(
               child: const Text("Ok"),
@@ -60,55 +78,127 @@ class _LoginState extends State<Login> {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (BuildContext context) => const ListCustomers(),
+        builder: (_) => const ListCustomers(),
       ),
     );
+  }
+
+  registrar() async {
+    // setState(() => loading = true);
+    // try {
+    //   await context.read<AuthService>().registrar(email.text, senha.text);
+    // } on AuthException catch (e) {
+    //   setState(() => loading = false);
+    //   ScaffoldMessenger.of(context)
+    //       .showSnackBar(SnackBar(content: Text(e.message)));
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[100],
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Form(
+            key: formKey,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const TextField(
-                  autofocus: true,
-                  keyboardType: TextInputType.number,
-                  style: TextStyle(color: Colors.blue, fontSize: 30),
-                  decoration: InputDecoration(
-                    labelText: "Usuário",
-                    labelStyle: TextStyle(color: Colors.black),
+              children: [
+                Text(
+                  titulo,
+                  style: const TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -1.5,
                   ),
-                ), //TextField
-                const Divider(),
-                const TextField(
-                    autofocus: true,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: TextFormField(
+                    controller: user,
+                    style: TextStyle(color: Colors.blue, fontSize: 30),
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Usuário",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Informe seu usuário";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 24.0),
+                  child: TextFormField(
+                    controller: senha,
                     obscureText: true,
-                    keyboardType: TextInputType.text,
+                    keyboardType: TextInputType.number,
                     style: TextStyle(color: Colors.blue, fontSize: 30),
                     decoration: InputDecoration(
+                      border: OutlineInputBorder(),
                       labelText: "Senha",
-                      labelStyle: TextStyle(color: Colors.black),
-                    )), //TextField
-                const Divider(),
-                ElevatedButton(
-                  onPressed: geraTokenProtheus,
-                  child: const Text(
-                    "Entrar",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w900,
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Informa sua senha!";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        if (isLogin) {
+                          login();
+                        } else {
+                          registrar();
+                        }
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: (loading)
+                          ? [
+                              Padding(
+                                padding: EdgeInsets.all(16),
+                                child: SizedBox(
+                                  width: 24,
+                                  height: 24,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              )
+                            ]
+                          : [
+                              Icon(Icons.check),
+                              Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Text(
+                                  actionButton,
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ],
                     ),
                   ),
-                )
+                ),
+                TextButton(
+                  onPressed: () => setFormAction(!isLogin),
+                  child: Text(""),
+                ),
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
